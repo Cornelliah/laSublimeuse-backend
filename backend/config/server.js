cb845f12752a965fe77487ea23a05b8502447395
Cornelliah <horianehkr@yahoo.com>
2025-08-29 20:55:22 +0100

    app

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
    app: {
-    keys: env.array('APP_KEYS', ['cgcuiunnvhgvg','qedfyuybjl','cyvhjolkvhk','fgbvjbknbl']), 
+    keys: env.array('APP_KEYS'), 
   },
   config: {
       jwtSecret: env('JWT_SECRET'),
