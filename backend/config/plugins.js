cfeaadf570f04414427637480622da4f19477219
Cornelliah <horianehkr@yahoo.com>
2025-08-30 07:26:10 +0100

    cloudinary

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,7 +1,26 @@
 module.exports = ({ env }) => ({
   'users-permissions': {
     config: {
-      jwtSecret: env('JWT_SECRET', '01857bca5c60daa172abd1e41b9ba9c59e6d048bde290c06ff2aba6ce235fda1'),
+      jwtSecret: env(
+        'JWT_SECRET',
+        '01857bca5c60daa172abd1e41b9ba9c59e6d048bde290c06ff2aba6ce235fda1'
+      ),
+    },
+  },
+
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
   },
 });
+
