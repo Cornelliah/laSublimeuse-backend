9c2629851bc2a01a9a6520549f67d75f4c5695b4
Cornelliah <horianehkr@yahoo.com>
2025-08-31 16:37:23 +0100

    logo

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,8 +2,7 @@ module.exports = ({ env }) => ({
   'users-permissions': {
     config: {
       jwtSecret: env(
-        'JWT_SECRET',
-        '01857bca5c60daa172abd1e41b9ba9c59e6d048bde290c06ff2aba6ce235fda1'
+        'JWT_SECRET'
       ),
     },
   },
