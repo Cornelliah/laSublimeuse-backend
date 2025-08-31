a9af0fff3be396c00b4fca8e02ece3c8a6024411
Cornelliah <horianehkr@yahoo.com>
2025-08-31 15:39:38 +0100

    logo

--- backend/src/admin/app.js
+++ backend/src/admin/app.js
@@ -2,13 +2,13 @@
 export default {
   config: {
     auth: {
-      logo: '../../public/uploads/lg.png',
+      logo: '../../public/uploads/lg_f2df491606.png',
     },
     menu: {
-      logo: '../../public/uploads/lg.png',
+      logo: '../../public/uploads/lg_f2df491606.png',
     },
     head: {
-      favicon: '../../public/uploads/lg.png',
+      favicon: '../../public/uploads/lg_f2df491606.png',
     },
   },
   bootstrap(app) {
